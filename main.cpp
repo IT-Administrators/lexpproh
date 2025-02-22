93c6719b1d8c25eca1d64f973f4ed28b7814f262
IT-Administrators <91905626+IT-Administrators@users.noreply.github.com>
2025-02-22 10:21:56 +0100

    Added cli interface
    Added cli interface to process all files in a directory
    or just a single file.

--- main.cpp
+++ main.cpp
@@ -9,27 +9,62 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
 
-    // Read files from specified directory.
-    std::string path = "./examples/";
-    for (const auto & entry : std::filesystem::directory_iterator(path)) {
-        
-        std::cout << std::endl;
-        std::cout << "PROCESSING FILE: " << entry.path() << std::endl;
-        
-        std::ifstream sourcefilestream(entry.path());
-        std::stringstream buffer;
-        char temp;
-
-        while (sourcefilestream.get(temp))
+    // Check if more than two arguments are provided.
+    if (argc > 2)
+    {
+        // Show wrogn arguments.
+        std::cerr << "Too many arguments: ";
+        for (size_t i = 2; i < argc; i++)
         {
-            buffer << temp;
+            std::cerr << argv[i] << " ";
         }
-        std::string sourceCode = buffer.str();
-        Lexer lexer(sourceCode);
-        std::vector<Token> tokens = lexer.tokenize();
-        PrintTokens(tokens);
+        std::cout << std::endl;
+        exit(1);
+    }
+    else
+    {
+        // Check if provided argument is a directory. And lex all files.
+        if (std::filesystem::is_directory(argv[1]))
+        {
+            for (const auto & entry : std::filesystem::directory_iterator(argv[1])) {
+        
+                std::cout << std::endl;
+                std::cout << "PROCESSING FILE: " << entry.path() << std::endl;
+                
+                std::ifstream sourcefilestream(entry.path());
+                std::stringstream buffer;
+                char temp;
+        
+                while (sourcefilestream.get(temp))
+                {
+                    buffer << temp;
+                }
+                std::string sourceCode = buffer.str();
+                Lexer lexer(sourceCode);
+                std::vector<Token> tokens = lexer.tokenize();
+                PrintTokens(tokens);
+            }
+        }
+        else
+        {
+            // If provided argument is a file. Lex only provided file.
+            std::cout << "PROCESSING FILE: " << argv[1] << std::endl;
+                
+            std::ifstream sourcefilestream(argv[1]);
+            std::stringstream buffer;
+            char temp;
+    
+            while (sourcefilestream.get(temp))
+            {
+                buffer << temp;
+            }
+            std::string sourceCode = buffer.str();
+            Lexer lexer(sourceCode);
+            std::vector<Token> tokens = lexer.tokenize();
+            PrintTokens(tokens);
+        } 
     }
 
     return 0;
